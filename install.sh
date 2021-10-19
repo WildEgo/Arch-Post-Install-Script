745e3f9d4bd7a8cae7339532b080ae39e9c94fd6
Luis Bizarro <ytgonee@gmail.com>
2021-10-19 19:10:42 +0100

    add: fisher

--- install.sh
+++ install.sh
@@ -5,11 +5,11 @@ git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 yes | makepkg -si
 cd ..
-yay --needed --noconfirm -S nvidia xorg-server egl-wayland wget git lightdm lightdm-gtk-greeter adobe-source-code-pro-fonts plasma-desktop kscreen plasma-wayland-session dolphin neofetch exa alsa-utils kitty-git fish nginx-mainline starship-git php-fpm python mariadb docker code filezilla android-studio google-chrome firefox opera lutris legendary steam vlc spotify ufw obs-studio virtualbox ffmpeg ktorrent timeshift-bin kdeconnect htop beekeeper-studio-bin go
+yay --needed --noconfirm -S nvidia xorg-server egl-wayland wget git lightdm lightdm-gtk-greeter adobe-source-code-pro-fonts plasma-desktop kscreen plasma-wayland-session dolphin neofetch exa alsa-utils kitty-git fish fisher nginx-mainline starship-git php-fpm composer python mariadb docker code filezilla android-studio google-chrome firefox opera lutris legendary steam vlc spotify ufw obs-studio virtualbox ffmpeg ktorrent timeshift-bin kdeconnect htop beekeeper-studio-bin go
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
 
-# Fish and aliases
+# Aliases
 alias ls="exa"
 
 # Post package commands
