bcd87818ee95dd49bbc170b11622a791147341bc
Luis Bizarro <ytgonee@gmail.com>
2021-10-19 18:01:04 +0100

    change: Fish and aliases moved above package cmds.

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ yes | makepkg -si
 cd ..
 yay --needed --noconfirm -S nvidia xorg-server egl-wayland wget git lightdm lightdm-gtk-greeter adobe-source-code-pro-fonts plasma-desktop kscreen plasma-wayland-session neofetch exa alsa-utils kitty-git fish nginx-mainline starship-git nvm php-fpm python mariadb docker code filezilla android-studio google-chrome firefox opera lutris legendary steam vlc spotify ufw obs-studio virtualbox ffmpeg ktorrent timeshift-bin kdeconnect htop beekeeper-studio-bin go
 
+# Fish and aliases
+omf install composer
+omf install nvm
+alias ls="exa"
+
 # Post package commands
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
 nvm install node
 nvm use node
 npm install --global yarn
 curl -L https://get.oh-my.fish | fish
-
-# Fish and aliases
-omf install composer
-omf install nvm
-alias ls="exa"
 
 # Move config files to correct directory
 mv -v ./config/* ~/.config
